98d685428a1bf1dcde9367e612f9174487780362
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-07-17 18:42:36 +0530

    updated the code to class based views with using mixins

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/', views.SnippetList.as_view()),
 ]
+
 
 urlpatterns = format_suffix_patterns(urlpatterns)
